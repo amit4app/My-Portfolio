dd8547d629cce607bc0a887492975ca5bf0b6461
AmitabhRithwan <amit4app@gmail.com>
2026-05-07 15:29:25 +0530

    Setup GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",   // IMPORTANT for GitHub User Pages
+  base: '/My-Portfolio/',
   plugins: [react()],
 })
